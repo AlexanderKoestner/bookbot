2e290fe37b5905847ce2f97967fb18b93f9f8e13
AlexanderKoestner <alexander.koestner87@gmail.com>
2024-02-15 10:50:04 +0100

    Finished bookbot course

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-            print(file_contents)
-            
+
+    aggregateReport("books/frankenstein.txt")
+    
+def aggregateReport(pathString):
+
+    content = openFile(pathString)
+
+    print(f"--- Begin report of {pathString} ---")
+    print(f"{numberOfWords(content)} words found in the document")
+    print("")
+
+    listOfDicts = []
+
+    for dicts in allChars(content):
+        if dicts.isalpha():
+            listOfDicts.append({"char": dicts, "num": allChars(content)[dicts]})
+
+    listOfDicts.sort(reverse = True, key = sort_on)
+
+    for value in listOfDicts:
+        print(f"The '{value["char"]}' character was found {value["num"]} times")
+
+    print("--- End report ---")
+
+def sort_on(dict):
+    return dict["num"]
+
+def numberOfWords(contentString):
+    return len(contentString.split())
+
+def allChars(contentString):
+    charDict = {}
+    for char in contentString.lower():
+        if char in charDict:
+            charDict[char] += 1
+        else:
+            charDict[char] = 1
+    return charDict
+
+def openFile(pathString):
+    with open(pathString) as f:
+        content = f.read()
+    return content
+
 main()
